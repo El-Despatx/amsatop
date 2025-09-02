0b6ff52db2fefb7e92cbb40c34472f1ad6f53e30
Oriol Agost Batalla <github@agost.info>
2025-09-02 11:47:46 +0200

    refactor: instantiate instance on method

--- src/amsatop/_ui/__init__.py
+++ src/amsatop/_ui/__init__.py
@@ -3,15 +3,18 @@ from amsatop._htop.htop_mock import HtopMock
 from amsatop._ui.tui import HtopTUI
 
 
-def run_ui(htop: Htop = HtopMock(), refresh: int = 2) -> None:
+def run_ui(htop: Htop = None, refresh: int = 2) -> None:
     """
     Given an instance of htop, run the tui interface.
     By default, if no parameter is given, it launches the HtopMock implementation.
-    
+
     :param htop: An instance of your htop implementation
     :param refresh: The seconds between each table refresh, 2 by default
     """
+    if htop is None:
+        htop = HtopMock()
     app = HtopTUI(htop=htop, refresh_seconds=refresh)
     app.run()
+
 
 __all__ = ["run_ui"]
