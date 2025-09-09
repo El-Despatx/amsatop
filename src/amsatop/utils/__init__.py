15c70f8dbb077015396090b408c53fa5d66fed66
Pablo Fraile Alonso <pablo@pablofraile.net>
2025-09-09 10:17:36 +0200

    refactor: don't make all submodules public

--- src/amsatop/utils/__init__.py
+++ src/amsatop/utils/__init__.py
@@ -2,7 +2,7 @@
 This module contains logic for making you easier to implement amsatop
 """
 
-from amsatop.utils.stat_file import StatFile, get_stat_file_from_path
-from amsatop.utils.status_file import StatusFile, get_status_file_from_path
+from amsatop.utils.__stat_file import StatFile, get_stat_file_from_path
+from amsatop.utils.__status_file import StatusFile, get_status_file_from_path
 
 __all__ = ["StatusFile", "StatFile", "get_status_file_from_path", "get_stat_file_from_path"]
